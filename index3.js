f5b2bc4e469eea020fd43bb298e342fccf6a6dc7
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-20 10:42:52 -0300

    Add: repetition structure

---
+++ index3.js
@@ -0,0 +1,9 @@
+let n = 5;
+
+/*Estrutura de repetição (Laços). Repete um bloco de código incontáveis vezes até que 
+a condição seja falsa*/
+for (let i = 0; i <= 10; i++) {
+
+    console.log(`${i} X ${n} = ${i*n}`);
+
+}
