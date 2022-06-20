52856cd22c6de5d28a116cb0474294b117ce1e40
Matheus Khairallah <61334878+MatheusKhairallah@users.noreply.github.com>
2022-06-20 09:51:19 -0300

    Add: variables and comparison operators

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Olá mundo");
+//Um igual "=" é atribuição de uma váriavel
+let a = 10;
+
+const b = "10";
+
+//Dois iguais "==" é comparação de valor de váriaveis
+console.log(a == b);
+
+//Três iguais "===" é comparação de valor e igualdade de váriaveis
+console.log(a === b);
+
+//Esclamação e dois iguais "!==" é comparação de diferença de valor de váriaveis
+console.log(a !== b);
+
+//Compara se as duas condições são verdadeiras
+console.log(a == b && typeof a == "string");
+
+//Compara se pelomenos uma das condições são verdadeiras
+console.log(a === b || typeof a == "number");
